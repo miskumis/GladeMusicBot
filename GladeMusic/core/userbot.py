410c7fc49ffc416ce3109b2dc5b5cb9916240e3a
rikodwi <riko.aa.aa.aa.aa@gmail.com>
2022-08-30 07:13:15 +0700

    p

--- GladeMusic/core/userbot.py
+++ GladeMusic/core/userbot.py
@@ -57,7 +57,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("mobile_legends_unofficial")
+                await self.one.join_chat("mobilelegendsbangbangunofficial")
             except:
                 pass
             assistants.append(1)
@@ -86,7 +86,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("mobile_legends_unofficial")
+                await self.two.join_chat("mobilelegendsbangbangunofficial")
             except:
                 pass
             assistants.append(2)
@@ -115,7 +115,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("mobile_legends_unofficial")
+                await self.three.join_chat("mobilelegendsbangbangunofficial")
             except:
                 pass
             assistants.append(3)
@@ -144,7 +144,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("mobile_legends_unofficial")
+                await self.four.join_chat("mobilelegendsbangbangunofficial")
             except:
                 pass
             assistants.append(4)
